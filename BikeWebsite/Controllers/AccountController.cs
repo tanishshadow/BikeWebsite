707beaa1f9e89129cbfadca7e9f75ba80906a3c1
jack-burns <1354796@15P327-S15-12.studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2015-11-11 13:59:47 -0500

    Login
    Can now login to the website. If a MANAGER logs in they can see the Bike
    Manager page.

---
+++ BikeWebsite/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BikeWebsite.Controllers
+{
+    public class AccountController : Controller
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.isValid(user.UserName, user.Password))
+                {
+                    var type = (from t in db.System_Users where t.Email == user.UserName select t.Type).ToList().First();
+                    string Type = type.ToString();
+                    user.Type = Type;
+                    FormsAuthentication.SetAuthCookie(user.UserName, user.RememberMe);
+                    Session["type"] = user.Type;
+                    Session["name"] = user.UserName;
+                    return RedirectToAction("Index", "Bikes");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login is Incorrect");
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Bikes");
+        }
+    }
+}
---
+++ BikeWebsite/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BikeWebsite.Models
+{
+    public class User
+    {
+        private DBEntities db = new DBEntities();
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me on this computer")]
+        public bool RememberMe { get; set; }
+
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Checks if user with given password exists in the database
+        /// </summary>
+        /// <param name="_username">User name</param>
+        /// <param name="_password">User password</param>
+        /// <returns>True if user exist and password is correct</returns>
+        public bool isValid(string _username, string _password)
+        {
+            var valid = (from v in db.System_Users
+                         where v.Email == _username && v.Password == _password
+                         select v.Email).ToList();
+            if (valid.Count() == 0)
+                return false;
+            if (valid[0].ToString() == _username)
+                return true;
+            return false;
+        }
+    }
+}
